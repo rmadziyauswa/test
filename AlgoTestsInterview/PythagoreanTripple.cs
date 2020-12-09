97c901b1fc5707a4981e8892f33ea9fe61adc5c1
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2020-12-09 14:33:57 +0200

    Pythagorean Solution and Unit Test

---
+++ AlgoTestsInterview/PythagoreanTripple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AlgoTestsInterview
+{
+    public class PythagoreanTripple
+    {
+        public int[] tripleSquareSum(int[] a)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < a.Length -2; i++)
+            {
+                result.Add(isPythagoreanTripple(a[i], a[i + 1], a[i + 2]));
+            }
+
+            return result.ToArray<int>();
+        }
+
+        private int isPythagoreanTripple(int a,int b,int c)
+        {
+            if(
+                Math.Pow(a,2) + Math.Pow(b,2) == Math.Pow(c,2) ||
+                Math.Pow(a,2) + Math.Pow(c,2) == Math.Pow(b,2) ||
+                Math.Pow(c,2) + Math.Pow(b,2) == Math.Pow(a,2)
+                )
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ UnitTestsForAlgoTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+using AlgoTestsInterview;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UnitTestsForAlgoTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PythagoreanTripple_Test()
+        {
+            //Arrange
+            var p = new PythagoreanTripple();
+            var a = new int[] { 0, 5, 5, 0, 5, 13, 12 };
+            var expectedAnswer = new int[] { 1, 1, 1, 0, 1 };
+
+            //Act
+            var actualAnswer = p.tripleSquareSum(a);
+
+            //Assert
+            Assert.IsTrue(expectedAnswer.SequenceEqual(actualAnswer));
+        }
+    }
+}
